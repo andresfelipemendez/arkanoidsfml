9ac0716d7c960861ba7f46f00bc6e0cc55a812a6
Andres Felipe Mendez <fmendez1212@gmail.com>
2017-04-02 16:21:14 -0500

    ball

--- arkanoid/Main.cpp
+++ arkanoid/Main.cpp
@@ -5,9 +5,41 @@ using namespace std;
 using namespace sf;
 
 constexpr unsigned int windowWidth{ 800 }, windowHeight{ 600 };
+constexpr float ballRadius{ 10.f }, ballVelocity{8.f};
+struct Ball
+{
+	CircleShape shape;
 
+	Vector2f velocity{ -ballVelocity, -ballVelocity };
+
+	Ball(float mX, float mY)
+	{
+		shape.setPosition(mX, mY);
+		shape.setRadius(ballRadius);
+		shape.setFillColor(Color::Red);
+		shape.setOrigin(ballRadius, ballRadius);
+	}
+
+	void update()
+	{
+		shape.move(velocity);
+		if (left() < 0) velocity.x = ballVelocity;
+		else if (right() > windowWidth) velocity.x = -ballVelocity;
+
+		if (top() < 0) velocity.y = ballVelocity;
+		else if (bottom() > windowHeight) velocity.y = -ballVelocity;
+	}
+
+	float x() { return shape.getPosition().x; }
+	float y() { return shape.getPosition().y; }
+	float left() { return x() - shape.getRadius(); }
+	float right() { return x() + shape.getRadius(); }
+	float top() { return y() - shape.getRadius(); }
+	float bottom() { return y() + shape.getRadius(); }
+};
 int main(int argc, char** argv) 
 {
+	Ball ball{ windowWidth / 2,windowHeight / 2 };
 	RenderWindow window{ { windowWidth, windowHeight }, "Arkanoid - 1" };
 	window.setFramerateLimit(60);
 
@@ -15,6 +47,8 @@ int main(int argc, char** argv)
 	{
 		window.clear(Color::Black);
 		if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
+		ball.update();
+		window.draw(ball.shape);
 		window.display();
 	}
 }
